1f6e93aefb802a8c38b1261329d10f82d5366066
HiepPD <hieppdutck59@gmail.com>
2023-04-26 21:30:02 +0700

    first commit

---
+++ app/src/main/java/com/example/customview/MainAct.kt
@@ -0,0 +1,12 @@
+package com.example.customview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_main)
+
+    }
+}
